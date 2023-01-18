68c250fe67e16b9bbc62957bc6a6bd486a5da281
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-01-17 22:28:00 -0700

    Refactor: use sentence case in custom field names

--- backend/src/main/kotlin/org/conservationco/asanahire/domain/ManagerApplicant.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/domain/ManagerApplicant.kt
@@ -1,8 +1,10 @@
 package org.conservationco.asanahire.domain
 
 import com.asana.models.Attachment
+import org.conservationco.asana.asanaContext
 import org.conservationco.asana.serialization.AsanaSerializable
 import org.conservationco.asana.serialization.customfield.AsanaCustomField
+import org.conservationco.asanahire.util.applicantDeserializingFn
 
 /**
  * Data class for POSTing [OriginalApplicant] objects to a manager context.
@@ -11,11 +13,11 @@ data class ManagerApplicant(
     override var id: String = "",
     override var name: String = "",
     override var documents: Collection<Attachment> = emptyList(),
-    @AsanaCustomField("Preferred Name")     override var preferredName: String = "",
+    @AsanaCustomField("Preferred name")     override var preferredName: String = "",
     @AsanaCustomField("Pronouns")           override var pronouns: String = "",
     @AsanaCustomField("Pronouns (other)")   override var pronounsOther: String = "",
     @AsanaCustomField("Email")              override var email: String = "",
-    @AsanaCustomField("Phone Number")       override var phoneNumber: String = "",
+    @AsanaCustomField("Phone number")       override var phoneNumber: String = "",
     @AsanaCustomField("References")         override var references: String = "",
     @AsanaCustomField("Rating")             var hiringManagerRating: String = "",
     @AsanaCustomField("Interview stage")    var interviewStage: String = "",
