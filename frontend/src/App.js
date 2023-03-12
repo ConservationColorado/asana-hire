0dd2da045509e2776c71c827c444ba9d72c2d619
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-12 16:14:44 -0600

    Refactor: pass in login/logout behavior to LoginPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,34 +9,44 @@ import AllJobsPage from "./pages/AllJobsPage";
 import NotFound from "./components/ui/NotFound";
 import NewJobPage from "./pages/NewJobPage";
 import LoginPage from "./pages/LoginPage";
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import Notification from "./components/ui/Notification"
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [token, setToken] = useState("");
 
-    function handleLogin() {
+    function handleLoginSuccess(credentialResponse) {
         setIsAuthenticated(true);
-    };
+        setToken(credentialResponse);
+    }
+
+    function handleLoginError() {
+        return <Notification message="Couldn't authenticate you! Please try again."/>
+    }
 
     return (
-        <Layout>
-            <Routes>
-                {isAuthenticated ? (
-                    <>
-                        <Route path="/" element={<HomePage/>}/>
-                        <Route path="/jobs" element={<AllJobsPage/>}/>
-                        <Route path="/jobs/new" element={<NewJobPage/>}/>
-                        <Route path="/settings" element={<SettingsPage/>}/>
-                        <Route path="/help" element={<HelpPage/>}/>
-                        <Route path="*" element={<NotFound/>}/>
-                    </>
-                ) : (
-                    <Route
-                        path="/*"
-                        element={<LoginPage onLogin={handleLogin}/>}
-                    />
-                )}
-            </Routes>
-        </Layout>
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+            <Layout isAuthenticated={isAuthenticated}>
+                <Routes>
+                    {isAuthenticated ? (
+                        <>
+                            <Route path="/" element={<HomePage/>}/>
+                            <Route path="/jobs" element={<AllJobsPage/>}/>
+                            <Route path="/jobs/new" element={<NewJobPage/>}/>
+                            <Route path="/settings" element={<SettingsPage/>}/>
+                            <Route path="/help" element={<HelpPage/>}/>
+                            <Route path="*" element={<NotFound/>}/>
+                        </>
+                    ) : (
+                        <Route
+                            path="/*"
+                            element={<LoginPage onSuccess={handleLoginSuccess} onError={handleLoginError}/>}
+                        />
+                    )}
+                </Routes>
+            </Layout>
+        </GoogleOAuthProvider>
     );
 }
 
