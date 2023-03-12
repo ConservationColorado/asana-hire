b60c34551ce2ab94379a49ade14c804e2411f617
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-12 11:26:53 -0600

    Authentication: route unauthenticated users to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Route, Routes,} from 'react-router-dom';
+import React, {useState} from "react";
+import {Route, Routes} from 'react-router-dom';
 
 import SettingsPage from "./pages/SettingsPage";
 import HelpPage from "./pages/HelpPage";
@@ -8,38 +8,36 @@ import HomePage from "./pages/HomePage";
 import AllJobsPage from "./pages/AllJobsPage";
 import NotFound from "./components/ui/NotFound";
 import NewJobPage from "./pages/NewJobPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    function handleLogin() {
+        setIsAuthenticated(true);
+    };
+
     return (
         <Layout>
             <Routes>
-                <Route
-                    path="/"
-                    element={<HomePage/>}
-                />
-                <Route
-                    path="/jobs"
-                    element={<AllJobsPage/>}
-                />
-                <Route
-                    path="/jobs/new"
-                    element={<NewJobPage/>}
-                />
-                <Route
-                    path="/settings"
-                    element={<SettingsPage/>}
-                />
-                <Route
-                    path="/help"
-                    element={<HelpPage/>}
-                />
-                <Route
-                    path="*"
-                    element={<NotFound/>}
-                />
+                {isAuthenticated ? (
+                    <>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/jobs" element={<AllJobsPage/>}/>
+                        <Route path="/jobs/new" element={<NewJobPage/>}/>
+                        <Route path="/settings" element={<SettingsPage/>}/>
+                        <Route path="/help" element={<HelpPage/>}/>
+                        <Route path="*" element={<NotFound/>}/>
+                    </>
+                ) : (
+                    <Route
+                        path="/*"
+                        element={<LoginPage onLogin={handleLogin}/>}
+                    />
+                )}
             </Routes>
         </Layout>
     );
 }
 
-export default App
+export default App;
