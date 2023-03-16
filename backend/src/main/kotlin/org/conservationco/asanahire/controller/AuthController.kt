9bbc5a51979735569561f158b6c00e1a9b41b8c1
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-16 10:07:30 -0600

    Spring: update authentication controller mapping and cross-origin setting

--- backend/src/main/kotlin/org/conservationco/asanahire/controller/AuthController.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/controller/AuthController.kt
@@ -1,15 +1,18 @@
 package org.conservationco.asanahire.controller
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/auth")
+@CrossOrigin
 class AuthController {
 
-    @PostMapping("/auth/google")
-    fun exchangeAuthorizationCode(@RequestParam("code") code: String): ResponseEntity<Any> {
+    @RequestMapping("/google")
+    fun validateAndExchangeGoogleAuthorizationCode(@RequestParam("code") code: String): ResponseEntity<Any> {
         return ResponseEntity.ok().build()
     }
 
