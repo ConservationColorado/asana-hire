f9081b06371621e6c33508de200ba7252cd98dd4
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-25 11:25:14 -0600

    Spring Security: basic OAuth2 configuration - make app home page and error page public - require an active session for all other requests - allow logout and redirect to home page - require CSRF token in each request - return HTTP unauthorized on error

--- backend/src/main/kotlin/org/conservationco/asanahire/config/SecurityConfiguration.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/config/SecurityConfiguration.kt
@@ -1,8 +1,43 @@
 package org.conservationco.asanahire.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
-//@Configuration
-//@EnableWebSecurity
-//class SecurityConfiguration {
-//
-//}
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): OAuth2LoginConfigurer<HttpSecurity> {
+        return http
+            .authorizeHttpRequests { authConfig ->
+                authConfig
+                    .requestMatchers("/", "/error")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
+            .logout { logoutConfig ->
+                logoutConfig
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+            }
+            .csrf { csrfConfig ->
+                csrfConfig
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            }
+            .exceptionHandling { errorConfig ->
+                errorConfig
+                    .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
+            .oauth2Login()
+    }
+
+}
