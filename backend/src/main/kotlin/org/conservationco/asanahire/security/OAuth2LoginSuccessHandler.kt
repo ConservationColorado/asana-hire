fb775c0d70006699bbc1658df7b12d0e1a246fb4
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-26 18:13:52 -0600

    OAuth2: remove access token cookie from successful authentication response
    No need to send the access token back to the front end. We'll set a session cookie that consumers
    of this application use only to access this back channel; the back end will use the access token
    on behalf of the user, without having to expose it to a less secure front channel.

--- backend/src/main/kotlin/org/conservationco/asanahire/security/OAuth2LoginSuccessHandler.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/security/OAuth2LoginSuccessHandler.kt
@@ -2,11 +2,7 @@ package org.conservationco.asanahire.security
 
 import org.conservationco.asanahire.service.UserService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.web.server.WebFilterExchange
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
@@ -15,7 +11,6 @@ import java.net.URI
 
 @Component
 class OAuth2LoginSuccessHandler(
-    private val authorizedClientService: ReactiveOAuth2AuthorizedClientService,
     private val userService: UserService
 ) : ServerAuthenticationSuccessHandler {
 
@@ -24,33 +19,14 @@ class OAuth2LoginSuccessHandler(
         authentication: Authentication
     ): Mono<Void> {
         userService.onAuthenticationSuccess(authentication)
-        return authorizedClientService
-            .loadAuthorizedClient<OAuth2AuthorizedClient>(
-                (authentication as OAuth2AuthenticationToken).authorizedClientRegistrationId,
-                authentication.name
-            )
-            .flatMap { authorizedClient -> webFilterExchange.addAccessTokenCookie(authorizedClient) }
+        redirectUser(webFilterExchange)
+        return Mono.empty()
     }
 
-    private fun WebFilterExchange.addAccessTokenCookie(
-        authorizedClient: OAuth2AuthorizedClient
-    ): Mono<Void> {
-        // Create a cookie with the access token
-        val cookie =
-            ResponseCookie
-                .from("access_token", authorizedClient.accessToken.tokenValue)
-                .httpOnly(true)
-                .domain("http://localhost:3000")
-
-        // Add the cookie to the response headers
-        val headers = exchange.response.headers
-        headers.add("Set-Cookie", cookie.toString())
-
-        val response = exchange.response
+    private fun redirectUser(webFilterExchange: WebFilterExchange) {
+        val response = webFilterExchange.exchange.response
         response.statusCode = HttpStatus.TEMPORARY_REDIRECT
         response.headers.location = URI.create("http://localhost:3000")
-
-        return Mono.empty()
     }
 
 }
