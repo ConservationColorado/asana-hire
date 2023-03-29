2c08b733d6c09e8056c84d5b7a59249e77de729b
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-03-29 09:51:37 -0600

    Refactor: make type of Mono generic

--- backend/src/main/kotlin/org/conservationco/asanahire/util/Monos.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/util/Monos.kt
@@ -3,7 +3,7 @@ package org.conservationco.asanahire.util
 import reactor.core.publisher.Mono
 import java.util.logging.Logger
 
-internal fun handleSaveError(logger: Logger, it: Throwable): Mono<Void> {
+internal fun <T> handleSaveError(logger: Logger, it: Throwable): Mono<T> {
     logger.severe("An error occurred while saving to repository: ${it.message})")
     return Mono.empty()
 }
