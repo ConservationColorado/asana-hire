f394a272a14c0ee9cf41d9174a9ddcd1b3dd5d38
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-04-02 11:04:52 -0600

    Feature: simple log out controller for OAuth2

---
+++ backend/src/main/kotlin/org/conservationco/asanahire/controller/LogoutController.kt
@@ -0,0 +1,15 @@
+package org.conservationco.asanahire.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.WebSession
+
+@RestController
+@RequestMapping("/oauth2/logout")
+class OAuth2LogoutController {
+
+    @PostMapping
+    fun logout(session: WebSession) = session.invalidate()
+
+}
