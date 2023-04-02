dad2126d73b19e64fa7cde587e44fdc76290d871
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-04-01 23:13:09 -0600

    Refactor: pass in HTTP response directly

--- backend/src/main/kotlin/org/conservationco/asanahire/security/OAuth2LoginSuccessHandler.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/security/OAuth2LoginSuccessHandler.kt
@@ -2,6 +2,7 @@ package org.conservationco.asanahire.security
 
 import org.conservationco.asanahire.service.UserService
 import org.springframework.http.HttpStatus
+import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.server.WebFilterExchange
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
@@ -11,19 +12,18 @@ import java.net.URI
 
 @Component
 class OAuth2LoginSuccessHandler(
-    private val userService: UserService
+    private val userService: UserService,
 ) : ServerAuthenticationSuccessHandler {
 
     override fun onAuthenticationSuccess(
         webFilterExchange: WebFilterExchange,
         authentication: Authentication
     ): Mono<Void> {
-        redirectUser(webFilterExchange)
+        redirectUser(webFilterExchange.exchange.response)
         return userService.onAuthenticationSuccess(authentication)
     }
 
-    private fun redirectUser(webFilterExchange: WebFilterExchange) {
-        val response = webFilterExchange.exchange.response
+    private fun redirectUser(response: ServerHttpResponse) {
         response.statusCode = HttpStatus.TEMPORARY_REDIRECT
         response.headers.location = URI.create("http://localhost:3000")
     }
