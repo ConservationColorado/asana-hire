87af37cb32cf45e398e32415a6744a5f3d2b4dc0
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-04-02 14:05:53 -0600

    Refactor: call log out function from AuthContext

--- frontend/src/components/oauth/GoogleLogoutButton.js
+++ frontend/src/components/oauth/GoogleLogoutButton.js
@@ -1,19 +1,16 @@
 import {Button} from "flowbite-react";
-import {googleLogout} from '@react-oauth/google';
 import {useContext} from "react";
 import {AuthContext} from "../../components/oauth/AuthProvider";
 
 function GoogleLogoutButton() {
-
     const {logout} = useContext(AuthContext);
 
-    const handleClick = () => {
-        googleLogout();
+    function handleClick() {
         logout();
-    };
+    }
 
     return (
-        <Button color="gray" pill={true} className="g_id_signout" onClick={handleClick} >
+        <Button color="gray" pill={true} onClick={handleClick} >
             Sign out
         </Button>
     );
