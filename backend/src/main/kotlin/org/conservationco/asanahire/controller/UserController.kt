634130eeae0ea83f7e0dab99ee69910aabc4cd89
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-04-01 23:11:13 -0600

    Refactor: make all UserController methods use /user url

--- backend/src/main/kotlin/org/conservationco/asanahire/controller/UserController.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/controller/UserController.kt
@@ -3,12 +3,14 @@ package org.conservationco.asanahire.controller
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/user")
 class UserController {
 
-    @GetMapping("/user")
+    @GetMapping("/me")
     fun user(@AuthenticationPrincipal principal: OAuth2User): Map<String, *> =
         principal.attributes
 
