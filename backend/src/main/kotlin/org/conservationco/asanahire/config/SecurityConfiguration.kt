dc70bf1d24d4d65896190f93dce611d1b47f6661
OliverAbdulrahim <oliver.abdulrahim@gmail.com>
2023-04-02 10:59:38 -0600

    Refactor: clean up security filter beans and delegate to CSRF handler

--- backend/src/main/kotlin/org/conservationco/asanahire/config/SecurityConfiguration.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/config/SecurityConfiguration.kt
@@ -1,6 +1,5 @@
 package org.conservationco.asanahire.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
@@ -8,51 +7,34 @@ import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.csrf.ServerCsrfTokenRepository
-import org.springframework.web.cors.CorsConfiguration
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestHandler
 import org.springframework.web.cors.reactive.CorsConfigurationSource
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
-class SecurityConfiguration {
-
-    @Autowired
-    private lateinit var successHandler: ServerAuthenticationSuccessHandler
-
-    @Autowired
-    private lateinit var csrfTokenRepository: ServerCsrfTokenRepository
+class SecurityConfiguration(
+    private val successHandler: ServerAuthenticationSuccessHandler,
+    private val csrfTokenRepository: ServerCsrfTokenRepository,
+    private val corsConfiguration: CorsConfigurationSource,
+    private val csrfDelegate: ServerCsrfTokenRequestHandler,
+) {
 
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    internal fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         http
-            .authorizeExchange { authSpec ->
-                authSpec
+            .authorizeExchange {
+                it
                     .pathMatchers("/", "/login", "/error").permitAll()
                     .anyExchange().authenticated()
             }
-            .cors { corsSpec ->
-                corsSpec
-                    .configurationSource(corsConfigurationSource())
-            }
-            .csrf { csrfSpec ->
-                csrfSpec
+            .cors { it.configurationSource(corsConfiguration) }
+            .csrf {
+                it
                     .csrfTokenRepository(csrfTokenRepository)
+                    .csrfTokenRequestHandler(csrfDelegate::handle)
             }
-            .oauth2Login { oAuth2LoginSpec ->
-                oAuth2LoginSpec
-                    .authenticationSuccessHandler(successHandler)
-            }
+            .oauth2Login { it.authenticationSuccessHandler(successHandler) }
         return http.build()
-    }
-
-    private fun corsConfigurationSource(): CorsConfigurationSource {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowedOrigins = listOf("http://localhost:3000")
-        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        config.allowCredentials = true
-        source.registerCorsConfiguration("/**", config)
-        return source
     }
 
 }
