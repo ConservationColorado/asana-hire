a1464662b33658b955fc8d9ea0bd1edbe67f2892
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-11 21:12:39 -0600

    Refactor: use ReactiveOAuth2AuthorizedClientService to get current authorized client

--- backend/src/main/kotlin/org/conservationco/asanahire/service/UserService.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/service/UserService.kt
@@ -5,10 +5,12 @@ import org.conservationco.asanahire.model.user.valueOfIgnoreCase
 import org.conservationco.asanahire.repository.UserRepository
 import org.conservationco.asanahire.util.handleSaveError
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
@@ -19,7 +21,7 @@ import java.util.logging.Logger
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val authorizedClientService: OAuth2AuthorizedClientService
+    private val clientService: ReactiveOAuth2AuthorizedClientService,
 ) {
 
     private val logger = Logger.getLogger(UserService::class.qualifiedName)
@@ -35,6 +37,21 @@ class UserService(
             .onErrorResume { handleSaveError(logger, it) }
     }
 
+    internal fun getCurrentlyAuthenticatedAuthorizedClient() =
+        getCurrentAuthentication()
+            .flatMap { validateAuthenticationAsOAuth2(it) }
+            .flatMap {
+                clientService
+                    .loadAuthorizedClient<OAuth2AuthorizedClient>(it.authorizedClientRegistrationId, it.name)
+            }
+
+    internal fun getCurrentlyAuthenticatedUserEmail() =
+        oAuth2AuthenticationToken()
+            .map {
+                val principal = it.principal as OAuth2User
+                principal.attributes["email"] as String
+            }
+
     private fun saveNewUser(user: User) =
         saveUser(user, "Saved new user from ${user.provider} OAuth2")
 
@@ -48,29 +65,14 @@ class UserService(
             saveUser(newUser, "Updated existing user with ${existingUser.provider} OAuth2")
         else Mono.empty()
 
-    internal fun getCurrentlyAuthenticatedUserAccessToken(): String {
-        val oauthToken = oAuth2AuthenticationToken()
-        val clientRegistrationId = oauthToken.authorizedClientRegistrationId
-        val authorizedClient = authorizedClientService.loadAuthorizedClient<OAuth2AuthorizedClient>(
-            clientRegistrationId,
-            oauthToken.name
-        )
-        return authorizedClient.accessToken.tokenValue
-    }
+    private fun oAuth2AuthenticationToken(): Mono<OAuth2AuthenticationToken> =
+        getCurrentAuthentication()
+            .flatMap { validateAuthenticationAsOAuth2(it) }
 
-    internal fun getCurrentlyAuthenticatedUserEmail(): String {
-        val authentication = oAuth2AuthenticationToken()
-        val user = authentication.principal as OAuth2User
-        return user.attributes["email"] as String
-    }
-
-    private fun oAuth2AuthenticationToken(): OAuth2AuthenticationToken =
-        SecurityContextHolder
-            .getContext()
-            .authentication
-            ?.takeIf { it.isAuthenticated }
-            ?.let { it as OAuth2AuthenticationToken }
-            ?: throw IllegalStateException("User not authenticated or OAuth2 token not found.")
+    private fun validateAuthenticationAsOAuth2(auth: Authentication?) =
+        if (auth != null && auth.isAuthenticated && auth is OAuth2AuthenticationToken)
+            Mono.just(auth)
+        else Mono.error(OAuth2AuthenticationException("User not authenticated or OAuth2 token not found!"))
 
 }
 
@@ -81,3 +83,8 @@ private fun DefaultOidcUser.toUser(provider: String) =
         picture = picture,
         provider = valueOfIgnoreCase(provider.uppercase(Locale.getDefault()))
     )
+
+private fun getCurrentAuthentication() =
+    ReactiveSecurityContextHolder
+        .getContext()
+        .map(SecurityContext::getAuthentication)
