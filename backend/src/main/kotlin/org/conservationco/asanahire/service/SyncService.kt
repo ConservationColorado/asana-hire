7f8d878402ef43e988e634a6bad1ac2e6671d9b2
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-11 21:23:11 -0600

    Refactor: sync service to use new mail api

--- backend/src/main/kotlin/org/conservationco/asanahire/service/SyncService.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/service/SyncService.kt
@@ -1,9 +1,6 @@
 package org.conservationco.asanahire.service
 
 import com.asana.models.Project
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.conservationco.asana.asanaContext
 import org.conservationco.asanahire.exception.JobNotFoundException
 import org.conservationco.asanahire.exception.SyncNotFoundException
@@ -11,44 +8,63 @@ import org.conservationco.asanahire.model.applicant.InterviewApplicant
 import org.conservationco.asanahire.model.applicant.OriginalApplicant
 import org.conservationco.asanahire.model.asana.ApplicantSyncPair
 import org.conservationco.asanahire.model.job.Job
+import org.conservationco.asanahire.model.mail.GenericAddress
 import org.conservationco.asanahire.model.sync.RequestState.*
 import org.conservationco.asanahire.model.sync.SyncEvent
 import org.conservationco.asanahire.repository.JobRepository
 import org.conservationco.asanahire.repository.SyncEventRepository
 import org.conservationco.asanahire.util.*
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.concurrent.ConcurrentHashMap
 
 @Service
 class SyncService(
     private val jobRepository: JobRepository,
     private val syncEventRepository: SyncEventRepository,
-    private val mailer: JobMailService,
+    private val mailService: MailService,
 ) {
 
-    fun getAllSyncs() =
+    private val jobLocks = ConcurrentHashMap<Long, Any>()
+
+    internal fun getAllSyncs() =
         syncEventRepository
             .findAll()
             .collectList()
 
-    fun getSync(syncId: Long) =
+    internal fun getSync(syncId: Long) =
         syncEventRepository
             .findById(syncId)
             .switchIfEmpty(Mono.error(SyncNotFoundException(syncId)))
 
-    fun startSync(jobId: Long): Mono<SyncEvent> {
-        return syncEventRepository
-            .findByJobId(jobId)
-            .switchIfEmpty(launchNewSync(jobId))
-            .flatMap { handleExistingRequest(it) }
+    internal fun startSync(jobId: Long) =
+        Mono.defer {
+            acquireLock(jobId)
+                .then(syncEventRepository.findById(jobId))
+                .switchIfEmpty(launchNewSync(jobId))
+                .flatMap { handleExistingRequest(it) }
+                .doFinally { releaseLock(jobId) }
+        }
+
+    private fun acquireLock(jobId: Long): Mono<Void> {
+        val lock = jobLocks.computeIfAbsent(jobId) { Object() }
+        return Mono.create { sink ->
+            synchronized(lock) {
+                sink.success()
+            }
+        }
     }
 
-    private fun handleExistingRequest(request: SyncEvent): Mono<SyncEvent> {
-        return when (request.status) {
+    private fun releaseLock(jobId: Long) {
+        jobLocks.remove(jobId)
+    }
+
+    private fun handleExistingRequest(request: SyncEvent) =
+        when (request.status) {
             NOT_STARTED, IN_PROGRESS -> Mono.just(request)
             COMPLETE -> launchNewSync(request.jobId)
         }
-    }
 
     private fun launchNewSync(jobId: Long) =
         jobRepository
@@ -62,31 +78,21 @@ class SyncService(
             Project().apply { gid = job.applicationProjectId },
             Project().apply { gid = job.interviewProjectId }
         )
-        return if (snapshot.needsSyncing()) {
-            startProjectSync(job, snapshot)
-        } else {
-            completedEvent(job)
-        }
+        return if (snapshot.needsSyncing()) startProjectSync(job, snapshot)
+        else completedEvent(job)
     }
 
-    private fun startProjectSync(job: Job, snapshot: SyncedProjectsSnapshot): Mono<SyncEvent> {
-        return syncEventRepository
+    private fun startProjectSync(job: Job, snapshot: SyncedProjectsSnapshot) =
+        syncEventRepository
             .save(inProgressEvent(job))
-            .flatMap {
-                startSync(job, snapshot)
-                Mono.just(it)
-            }
-    }
+            .flatMap { startSync(job, snapshot).then(Mono.just(it)) }
 
-    private fun startSync(job: Job, snapshot: SyncedProjectsSnapshot) =
-        CoroutineScope(Dispatchers.IO).launch {
-            println(snapshot)
-            val (jobTitle, source, destination) = snapshot
-            val applicants = getNewApplicants(job, snapshot)
-            for (pair in applicants) {
-                syncSingleApplicant(pair, destination, jobTitle, source)
-            }
-        }
+    private fun startSync(job: Job, snapshot: SyncedProjectsSnapshot): Flux<Void> {
+        val (jobTitle, source, destination) = snapshot
+        val applicants = getNewApplicants(job, snapshot)
+        return Flux.fromIterable(applicants)
+            .flatMap { syncSingleApplicant(it, destination, jobTitle, source) }
+    }
 
     /**
      * For each new applicant:
@@ -101,33 +107,29 @@ class SyncService(
         applicantPair: ApplicantSyncPair,
         destination: Project,
         jobTitle: String,
-        source: Project
-    ) {
+        source: Project,
+    ): Mono<Void> {
         val (originalApplicant, interviewApplicant) = applicantPair
         addToInterviewProject(interviewApplicant, destination)
-        sendReceiptEmail(originalApplicant, jobTitle)
         originalApplicant.updateReceiptOfApplication(source)
+        return sendReceiptEmail(originalApplicant, jobTitle)
     }
 
-    private fun sendReceiptEmail(
-        originalApplicant: OriginalApplicant,
-        jobTitle: String
-    ) = CoroutineScope(Dispatchers.IO).launch {
-        mailer.emailReceiptOfApplication(
-            originalApplicant.preferredName,
-            originalApplicant.email,
-            jobTitle
-        )
+    private fun sendReceiptEmail(applicant: OriginalApplicant, jobTitle: String): Mono<Void> {
+        val recipient = GenericAddress(applicant.name, applicant.email)
+        val message = mailService.createUpdateEmailBody(recipient, jobTitle)
+        return mailService
+            .createMessage(recipient, message)
+            .flatMap { mailService.send(it) }
+            .then()
     }
 
     private fun addToInterviewProject(
         interviewApplicant: InterviewApplicant,
         destination: Project
-    ) {
-        asanaContext {
-            val task = interviewApplicant.convertToTask(destination, applicantSerializingFn())
-            destination.createTask(task)
-        }
+    ) = asanaContext {
+        val task = interviewApplicant.convertToTask(destination, applicantSerializingFn())
+        destination.createTask(task)
     }
 
     /**
