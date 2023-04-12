fc39dea1a5bca8e4332d075f2132cb403b5befcf
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-11 21:18:26 -0600

    Refactor: applicant service and controller layers to reactive and new mail api

--- backend/src/main/kotlin/org/conservationco/asanahire/controller/ApplicantController.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/controller/ApplicantController.kt
@@ -3,6 +3,7 @@ package org.conservationco.asanahire.controller
 import org.conservationco.asanahire.model.applicant.RejectableApplicant
 import org.conservationco.asanahire.service.ApplicantService
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import java.time.LocalDateTime
 
 @RestController
@@ -12,24 +13,26 @@ class ApplicantController(
 ) {
 
     @GetMapping("/{jobId}/new")
-    suspend fun getNewApplicants(
+    fun getNewApplicants(
         @PathVariable jobId: Long,
         @RequestBody time: LocalDateTime
     ) = applicantService.getNewApplicants(jobId, time)
 
     @GetMapping("/{jobId}/reject")
-    suspend fun getRejectableApplicants(
+    fun getRejectableApplicants(
         @PathVariable jobId: Long
     ) = applicantService.getAllNeedingRejection(jobId)
 
     @PutMapping("/reject")
-    suspend fun rejectApplicant(
+    fun rejectApplicant(
         @RequestBody applicant: RejectableApplicant
     ) = applicantService.rejectApplicant(applicant)
 
     @PutMapping("/reject-all")
-    suspend fun rejectApplicants(
+    fun rejectApplicants(
         @RequestBody applicants: List<RejectableApplicant>
-    ) = applicants.forEach { applicantService.rejectApplicant(it) }
+    ) = Flux
+        .fromIterable(applicants)
+        .flatMap { applicantService.rejectApplicant(it) }
 
 }
--- backend/src/main/kotlin/org/conservationco/asanahire/service/ApplicantService.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/service/ApplicantService.kt
@@ -2,14 +2,13 @@ package org.conservationco.asanahire.service
 
 import com.asana.models.Task
 import com.asana.models.Workspace
-import kotlinx.coroutines.*
 import org.conservationco.asana.asanaContext
 import org.conservationco.asana.util.AsanaTable
 import org.conservationco.asanahire.model.applicant.*
 import org.conservationco.asanahire.model.job.Job
+import org.conservationco.asanahire.model.mail.GenericAddress
 import org.conservationco.asanahire.repository.JobRepository
 import org.conservationco.asanahire.util.*
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
@@ -17,40 +16,43 @@ import java.time.LocalDateTime
 @Service
 class ApplicantService(
     private val jobRepository: JobRepository,
-    @Autowired private val mailer: JobMailService,
-    @Autowired private val workspace: Workspace,
+    private val mailService: MailService,
+    private val workspace: Workspace,
 ) {
 
-    private val applicantScope = CoroutineScope(SupervisorJob())
-
-    fun getNewApplicants(jobId: Long, time: LocalDateTime): Deferred<List<ApplicantEvent>> = asanaContext {
-        return applicantScope.async {
-            workspace
-                .search(
-                    "created_at.after" to time.toString(),
-                    "projects.any" to jobId.toString()
-                )
-                .map(Task::toApplicantEvent)
+    internal fun getNewApplicants(jobId: Long, time: LocalDateTime): Mono<List<ApplicantEvent>> =
+        asanaContext {
+            Mono.fromCallable {
+                workspace
+                    .search(
+                        "created_at.after" to time.toString(),
+                        "projects.any" to jobId.toString()
+                    )
+                    .map(Task::toApplicantEvent)
+            }
         }
-    }
 
-    fun getAllNeedingRejection(jobId: Long): Mono<List<RejectableApplicant>> =
+    internal fun getAllNeedingRejection(jobId: Long): Mono<List<RejectableApplicant>> =
         jobRepository
             .findById(jobId)
             .map { rejectableApplicants(it) }
 
-    fun rejectApplicant(applicant: RejectableApplicant) =
+    internal fun rejectApplicant(applicant: RejectableApplicant) =
         jobRepository
             .findById(applicant.jobId)
-            .doOnSuccess { job ->
-                updateRejectionStatusFor(job, applicant)
-                sendRejectionEmail(applicant, job)
+            .flatMap {
+                updateRejectionStatusFor(it, applicant)
+                sendRejectionEmail(applicant, it.title)
             }
 
-    private fun sendRejectionEmail(applicant: RejectableApplicant, job: Job) =
-        applicantScope.launch {
-            mailer.emailRejection(applicant.preferredName, applicant.email, job.title)
-        }
+    private fun sendRejectionEmail(applicant: RejectableApplicant, jobTitle: String): Mono<Void> {
+        val recipient = GenericAddress(applicant.name, applicant.email)
+        val message = mailService.createRejectionEmailBody(recipient, jobTitle)
+        return mailService
+            .createMessage(recipient, message)
+            .flatMap { mailService.send(it) }
+            .then()
+    }
 
     private fun updateRejectionStatusFor(
         job: Job,
