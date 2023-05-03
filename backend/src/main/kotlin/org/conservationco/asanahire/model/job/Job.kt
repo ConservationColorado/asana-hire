81fa7fc5f328cf145bc8c43858597fdf5839c304
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-05-02 22:00:06 -0600

    DB: add auto syncing column to Job

--- backend/src/main/kotlin/org/conservationco/asanahire/model/job/Job.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/model/job/Job.kt
@@ -7,8 +7,9 @@ import org.springframework.data.relational.core.mapping.Table
 @Table(name = "Job")
 data class Job(
     @Id
-    var id: Long = 0L,
-    var title: String = "",
-    var applicationProjectId: String = "",
-    var interviewProjectId: String = "",
+    val id: Long = 0L,
+    val title: String = "",
+    val applicationProjectId: String = "",
+    val interviewProjectId: String = "",
+    val isAutoSyncing: Boolean = false,
 ) : AsanaSerializable<Job>
