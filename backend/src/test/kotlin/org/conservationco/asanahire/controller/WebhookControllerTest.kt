7f139fef039968a23ed3fd6c419320436c99fa1e
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-05-15 23:35:08 -0600

    Tests: validate webhook request headers

--- backend/src/test/kotlin/org/conservationco/asanahire/controller/WebhookControllerTest.kt
+++ backend/src/test/kotlin/org/conservationco/asanahire/controller/WebhookControllerTest.kt
@@ -1,5 +1,9 @@
 package org.conservationco.asanahire.controller
 
+import org.conservationco.asanahire.config.asanaWebhookCreatePath
+import org.conservationco.asanahire.config.webhookSecretHeader
+import org.conservationco.asanahire.config.webhookSignatureHeader
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,5 +17,36 @@ import org.springframework.test.web.reactive.server.WebTestClient
 internal class WebhookControllerTest(
     @Autowired private val client: WebTestClient,
 ) {
+
+    @Test
+    fun `should allow X-Hook-Signature header`() {
+        client
+            .options()
+            .uri(asanaWebhookCreatePath)
+            .header(webhookSignatureHeader, "54321")
+            .exchange()
+            .expectStatus().isOk
+    }
+
+    @Test
+    fun `should require a request body on webhook create request`() {
+        client
+            .post()
+            .uri(asanaWebhookCreatePath)
+            .exchange()
+            .expectStatus().isBadRequest
+    }
+
+    @Test
+    fun `should return X-Hook-Secret header on webhook handshake initiation`() {
+        client
+            .post()
+            .uri(asanaWebhookCreatePath)
+            .header(webhookSecretHeader, "12345")
+            .bodyValue("")
+            .exchange()
+            .expectStatus().isNoContent
+            .expectHeader().valueMatches(webhookSecretHeader, "12345")
+    }
 
 }
