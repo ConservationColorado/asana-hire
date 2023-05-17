0e3b66f115c96aae6482c7fdf007a1c22114f16a
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-05-16 19:02:59 -0600

    Add Gson parser bean with ability to parse String > DateTime (Google)

--- backend/src/main/kotlin/org/conservationco/asanahire/config/DomainConfiguration.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/config/DomainConfiguration.kt
@@ -2,9 +2,12 @@ package org.conservationco.asanahire.config
 
 import com.asana.models.Portfolio
 import com.asana.models.Workspace
+import com.google.api.client.util.DateTime
+import com.google.gson.*
 import org.conservationco.asanahire.model.asana.JobSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.lang.reflect.Type
 
 @Configuration
 class DomainConfiguration {
@@ -17,6 +20,18 @@ class DomainConfiguration {
 
     @Bean
     fun workspace() = Workspace().apply { gid = env("ASANA_WORKSPACE_GID") }
+
+    @Bean
+    fun gson(): Gson = GsonBuilder()
+        .registerTypeAdapter(DateTime::class.java, DateTimeDeserializer())
+        .create()
+
+    private class DateTimeDeserializer : JsonDeserializer<DateTime> {
+        override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): DateTime {
+            val dateTimeString = json.asString
+            return DateTime(dateTimeString)
+        }
+    }
 
 }
 
