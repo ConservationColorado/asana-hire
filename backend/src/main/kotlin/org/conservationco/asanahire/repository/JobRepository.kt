0a47c06ae8843ee2328799ea55611aa0f4e84a6a
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-05-16 19:03:57 -0600

    Add functions to find jobs by project ids

--- backend/src/main/kotlin/org/conservationco/asanahire/repository/JobRepository.kt
+++ backend/src/main/kotlin/org/conservationco/asanahire/repository/JobRepository.kt
@@ -2,5 +2,9 @@ package org.conservationco.asanahire.repository
 
 import org.conservationco.asanahire.model.job.Job
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Mono
 
-interface JobRepository : ReactiveCrudRepository<Job, Long>
+interface JobRepository : ReactiveCrudRepository<Job, Long> {
+    fun findFirstByApplicationProjectId(id: String): Mono<Job>
+    fun findFirstByInterviewProjectId(id: String): Mono<Job>
+}
